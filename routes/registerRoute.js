9bfbd02fff5e8c9e8d99f94f32d79cad35f46a0c
ByronHolmes-at-129020800614 <lshopeful2018@gmail.com>
2021-03-18 08:06:18 -0500

    Added 'add user' functionality. Also checks to see if username or email is already in use.

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser");
+const User = require("../schemas/UserSchema");
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -12,7 +13,7 @@ router.get("/", (req, res, next) => {
   res.status(200).render("register");
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   // use .trim() -> takes spaces before and after the text in the field
   const firstName = req.body.firstName.trim();
   const lastName = req.body.lastName.trim();
@@ -24,6 +25,28 @@ router.post("/", (req, res, next) => {
   const payload = req.body;
 
   if (firstName && lastName && userName && email && password) {
+    // make sure no user with the same userName and email already exists
+    const user = await User.findOne({
+      $or: [{ userName: userName }, { email: email }],
+    }).catch((err) => {
+      console.log(err);
+      payload.errorMessage = "Something went wrong!";
+      res.status(200).render("register", payload);
+    });
+
+    if (user === null) {
+      // No user found, insert 'em.
+      const data = req.body;
+      User.create(data).then((user) => console.log(user));
+    } else {
+      // User found, tell 'em.
+      if (email === user.email) {
+        payload.errorMessage = "Email already in use";
+      } else {
+        payload.errorMessage = "Username already in use";
+      }
+      res.status(200).render("register", payload);
+    }
   } else {
     payload.errorMessage = "Make sure each field has valid value";
     res.status(200).render("register", payload);
