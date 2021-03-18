9a97c8a44dc526be21accf827b66b56b40bd4ca4
ByronHolmes-at-129020800614 <lshopeful2018@gmail.com>
2021-03-18 12:18:49 -0500

    Added hashing for pwd

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser");
 const User = require("../schemas/UserSchema");
+const bcrypt = require("bcrypt");
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -37,6 +38,8 @@ router.post("/", async (req, res, next) => {
     if (user === null) {
       // No user found, insert 'em.
       const data = req.body;
+
+      data.password = await bcrypt.hash(password, 10);
       User.create(data).then((user) => console.log(user));
     } else {
       // User found, tell 'em.
